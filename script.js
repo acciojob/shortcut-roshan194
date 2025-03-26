1555607cd8933d73fc344c64b56c0348bfcc46c8
Roshan <71066704+roshan194@users.noreply.github.com>
2025-03-26 11:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+    if (!str1 || !str2) {
+        return '';
+    }
+    return str1[0] + str2[0];
 }
 
 // Do not change the code below.
